ea1cafef8318492bffc83648d5491df95bb5fd0a
Ghostreaper57 <jeremy.nenich@live.fr>
2015-01-05 17:30:18 +0100

    Version -1
    Un debut de page d'accueil

---
+++ SMS114/app/src/androidTest/java/hupays_nenich/com/sms114/ApplicationTest.java
@@ -0,0 +1,13 @@
+package hupays_nenich.com.sms114;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
