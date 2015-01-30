e2b2ccb160407d940b1da5795a88f9843001345d
Binayre <txtrif@gmail.com>
2015-01-30 23:31:50 +0100

    0.9.5
    + envoi du sms au 114 (code)
    + icone de l appli un peu plus grande
    + logo à l'écran accueil

--- SMS114/app/src/main/java/hupays_nenich/com/sms114/message/EnvoiMessageListener.java
+++ SMS114/app/src/main/java/hupays_nenich/com/sms114/message/EnvoiMessageListener.java
@@ -1,6 +1,10 @@
 package hupays_nenich.com.sms114.message;
 
+import android.app.PendingIntent;
 import android.view.View;
+import android.telephony.SmsManager;
+import android.widget.Toast;
+import android.content.Intent;
 
 import hupays_nenich.com.sms114.Dialogue;
 
@@ -19,6 +23,30 @@ public class EnvoiMessageListener implements View.OnClickListener{
     public void onClick(View v) {
         Dialogue d = new Dialogue(activity,activity.getMessageFinal(),"test");
 
-        //envoyer le message et mettre une pop-up l'indiquant
+        // Envoi le message
+        String num = "114";
+        String msg = activity.getMessageFinal();
+        try {
+            SmsManager.getDefault().sendTextMessage(num, null, msg, null, null);
+            Toast.makeText(activity.getApplicationContext(), "SMS Envoyé!", Toast.LENGTH_LONG).show();
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(activity.getApplicationContext(),"L'envoi du SMS a échoué", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+
+        // Utiliser l application SMS du telephone
+        /*try {
+            Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+            sendIntent.putExtra(msg, "default content");
+            sendIntent.setType("vnd.android-dir/mms-sms");
+            startActivity(sendIntent);
+        }
+        catch(Exception e)
+        {
+            Toast.makeText(getApplicationContext(),"L'envoi du SMS a échoué", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }*/
     }
 }
